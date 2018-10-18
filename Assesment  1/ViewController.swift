960e21dfa35bedf7b7f9405ea8ba957509ec5bf2
Kaleb Spillman <kalebspillman@Interapts-MacBook-Pro-5.local>
2018-10-18 08:29:56 -0500

    first push

--- Assesment  1/ViewController.swift
+++ Assesment  1/ViewController.swift
@@ -15,6 +15,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBOutlet weak var label: UILabel!
+    @IBAction func textField(_ sender: Any) {
+   
+    }
+    @IBAction func button(_ sender: Any) {
+    }
 }
 
