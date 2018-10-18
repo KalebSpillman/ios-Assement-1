1ab5d6b0bedaaf98275fe4e9ad7baa59e75a7eeb
Kaleb Spillman <kalebspillman@Interapts-MacBook-Pro-5.local>
2018-10-18 08:20:19 -0500

    Initial Commit

---
+++ Assesment  1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assesment  1
+//
+//  Created by Kaleb Spillman on 10/18/18.
+//  Copyright © 2018 Kaleb Spillman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
